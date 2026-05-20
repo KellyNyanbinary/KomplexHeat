4dca55056f4d6f14e3b4dc6d940fdfc7ddcd0b21
KellyNyanbinary <43101126+KellyNyanbinary@users.noreply.github.com>
2026-05-19 18:27:29 -0600

    Add HeatController to increment temperature based on power
    - Make Vizzy part heating depend on power draw
    - Something causes the script to stop working a few second into flight with the KAFX-20C?

---
+++ Assets/Scripts/KomplexHeat/HeatController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Craft.Parts;
+using ModApi.Common;
+using ModApi.GameLoop;
+using ModApi.GameLoop.Interfaces;
+using UnityEngine;
+
+namespace KomplexHeat
+{
+    public class HeatController : MonoBehaviour, IFlightFixedUpdate
+    {
+        private const float SpecificHeatOfAl = 897f; // https://en.wikipedia.org/wiki/Aluminium
+
+        private static readonly Dictionary<PartScript, float> PendingWatts = new();
+
+        private void OnEnable()
+        {
+            Game.Loop.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            Game.Loop.Unregister(this);
+        }
+
+        public bool StartMethodCalled { get; set; }
+
+        public new int GetInstanceID()
+        {
+            return base.GetInstanceID();
+        }
+
+        void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame)
+        {
+            foreach (var (part, watts) in PendingWatts)
+            {
+                var tempIncrease = watts * frame.DeltaTime / (part.Data.Mass * SpecificHeatOfAl);
+                part.Temperature += tempIncrease;
+            }
+
+            PendingWatts.Clear();
+        }
+
+        public static void AddHeat(PartScript part, float watts)
+        {
+            if (PendingWatts.ContainsKey(part))
+                PendingWatts[part] += watts;
+            else
+                PendingWatts[part] = watts;
+        }
+    }
+}
