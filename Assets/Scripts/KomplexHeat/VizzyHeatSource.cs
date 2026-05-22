16e6336661943b722567580a92bbfa8cb0c5b074
KellyNyanbinary <43101126+KellyNyanbinary@users.noreply.github.com>
2026-05-21 21:44:22 -0600

    Revert "Add HeatController to increment temperature based on power"
    This reverts commit 4dca5505

--- Assets/Scripts/KomplexHeat/VizzyHeatSource.cs
+++ Assets/Scripts/KomplexHeat/VizzyHeatSource.cs
@@ -1,38 +1,39 @@
-﻿using System.Reflection;
-using Assets.Scripts.Craft.Parts;
+﻿using Assets.Scripts.Craft.Parts;
 using Assets.Scripts.Craft.Parts.Modifiers;
+using Assets.Scripts.Craft.Parts.Modifiers.Propulsion;
 using ModApi.GameLoop;
 using ModApi.GameLoop.Interfaces;
 
 namespace KomplexHeat
 {
-    public class VizzyHeatSource : MonoBehaviourBase, IFlightStart, IFlightFixedUpdate
+    public class VizzyHeatSource : MonoBehaviourBase, IHeatSource, IFlightStart, IFlightFixedUpdate
     {
-        private const float PowerMultiplier = 1000f; // The game uses kiloWatts, but we use Watts
+        private const float RunningTemperature = 340f; // Kelvin for ~67 degrees above ambient
+        private const float HeatTransferCoefficient = 5f;
+
         private FlightProgramScript _flightProgramScript;
-        private PartScript _partScript;
-        private FieldInfo _powerConsumptionField;
 
         void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame)
         {
             if (_flightProgramScript == null || _flightProgramScript.FlightProgram == null)
+            {
+                Temperature = 0f;
                 return;
-            
-            var powerConsumption = _powerConsumptionField != null
-                ? (float)_powerConsumptionField.GetValue(_flightProgramScript) * PowerMultiplier
-                : 0;
-            
-            UnityEngine.Debug.Log($"Power consumption for {_partScript.name}: {powerConsumption} W");
+            }
 
-            HeatController.AddHeat(_partScript, powerConsumption);
+            Temperature = RunningTemperature;
         }
 
         void IFlightStart.FlightStart(in FlightFrameData frame)
         {
             _flightProgramScript = GetComponentInParent<FlightProgramScript>();
-            _partScript = GetComponentInParent<PartScript>();
-            _powerConsumptionField = _flightProgramScript?.GetType()
-                .GetField("_powerConsumption", BindingFlags.NonPublic | BindingFlags.Instance);
         }
+
+        float IHeatSource.GetHeatTransferRate(PartScript partScript)
+        {
+            return HeatTransferCoefficient;
+        }
+
+        public float Temperature { get; private set; }
     }
 }
