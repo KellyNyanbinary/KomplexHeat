641043f15c2416f864033698bcb04a69e3191c54
KellyNyanbinary <43101126+KellyNyanbinary@users.noreply.github.com>
2026-05-22 23:19:19 -0600

    Refactor HeatController and VizzyHeatSource to work during warp

--- Assets/Scripts/KomplexHeat/HeatController.cs
+++ Assets/Scripts/KomplexHeat/HeatController.cs
@@ -1,27 +1,30 @@
 ﻿using System.Collections.Generic;
 using Assets.Scripts.Craft.Parts;
-using ModApi.Common;
 using ModApi.GameLoop;
 using ModApi.GameLoop.Interfaces;
 using UnityEngine;
 
 namespace KomplexHeat
 {
-    public class HeatController : MonoBehaviourBase, IFlightFixedUpdate
+    public class HeatController : MonoBehaviourBase, IFlightFixedUpdate, IFlightFixedUpdateWarp
     {
         private const float SpecificHeatOfAl = 897f; // https://en.wikipedia.org/wiki/Aluminium
 
         private static readonly Dictionary<PartScript, float> PendingWatts = new();
 
-        void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame)
+        void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame) => ApplyPendingHeat(frame);
+        void IFlightFixedUpdateWarp.FlightFixedUpdateWarp(in FlightFrameData frame) => ApplyPendingHeat(frame);
+
+        private void ApplyPendingHeat(in FlightFrameData frame)
         {
             Debug.Log($"Game tick {frame.FrameCount}, applying heat to {PendingWatts.Count} parts");
-            
+
             foreach (var (part, watts) in PendingWatts)
             {
                 var tempIncrease = watts * frame.DeltaTime / (part.Data.Mass * SpecificHeatOfAl) * 100;
                 part.Temperature += tempIncrease;
-                Debug.Log($"Added {watts} W to part {part.name} (ID: {part.GetInstanceID()}). New temperature: {part.Temperature}");
+                Debug.Log(
+                    $"Added {watts} W to part {part.name} (ID: {part.GetInstanceID()}). New temperature: {part.Temperature}");
             }
 
             PendingWatts.Clear();
@@ -35,7 +38,7 @@ namespace KomplexHeat
                 PendingWatts[part] += watts;
             else
                 PendingWatts[part] = watts;
-            
+
             Debug.Log($"Queue length: {PendingWatts.Count}");
         }
     }
--- Assets/Scripts/KomplexHeat/VizzyHeatSource.cs
+++ Assets/Scripts/KomplexHeat/VizzyHeatSource.cs
@@ -3,29 +3,20 @@ using Assets.Scripts.Craft.Parts;
 using Assets.Scripts.Craft.Parts.Modifiers;
 using ModApi.GameLoop;
 using ModApi.GameLoop.Interfaces;
+using UnityEngine;
 
 namespace KomplexHeat
 {
-    public class VizzyHeatSource : MonoBehaviourBase, IFlightStart, IFlightFixedUpdate
+    public class VizzyHeatSource : MonoBehaviourBase, IFlightStart, IFlightFixedUpdate, IFlightFixedUpdateWarp
     {
         private const float PowerMultiplier = 1000f; // The game uses kiloWatts, but we use Watts
         private FlightProgramScript _flightProgramScript;
         private PartScript _partScript;
         private FieldInfo _powerConsumptionField;
 
-        void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame)
-        {
-            if (_flightProgramScript == null || _flightProgramScript.FlightProgram == null)
-                return;
-            
-            var powerConsumption = _powerConsumptionField != null
-                ? (float)_powerConsumptionField.GetValue(_flightProgramScript) * PowerMultiplier
-                : 0;
-            
-            UnityEngine.Debug.Log($"Power consumption for {_partScript.name}: {powerConsumption} W");
+        void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame) => ApplyHeat(frame);
 
-            HeatController.AddHeat(_partScript, powerConsumption);
-        }
+        void IFlightFixedUpdateWarp.FlightFixedUpdateWarp(in FlightFrameData frame) => ApplyHeat(frame);
 
         void IFlightStart.FlightStart(in FlightFrameData frame)
         {
@@ -33,6 +24,20 @@ namespace KomplexHeat
             _partScript = GetComponentInParent<PartScript>();
             _powerConsumptionField = _flightProgramScript?.GetType()
                 .GetField("_powerConsumption", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        private void ApplyHeat(in FlightFrameData frame)
+        {
+            if (_flightProgramScript == null || _flightProgramScript.FlightProgram == null)
+                return;
+
+            var powerConsumption = _powerConsumptionField != null
+                ? (float)_powerConsumptionField.GetValue(_flightProgramScript) * PowerMultiplier
+                : 0;
+
+            Debug.Log($"Power consumption for {_partScript.name}: {powerConsumption} W");
+
+            HeatController.AddHeat(_partScript, powerConsumption);
         }
     }
 }
