250ef0b8df6ccc2fe42558b8950efc5a12bf3140
KellyNyanbinary <43101126+KellyNyanbinary@users.noreply.github.com>
2026-05-22 22:51:12 -0600

    Revert "Refactor VizzyHeatSource to implement IFlightUpdate instead of IFlightFixedUpdate"
    This reverts commit 6472ec8ef5d463a7e6158b02e9655c5803b21ce9.

--- Assets/Scripts/KomplexHeat/VizzyHeatSource.cs
+++ Assets/Scripts/KomplexHeat/VizzyHeatSource.cs
@@ -6,34 +6,27 @@ using ModApi.GameLoop.Interfaces;
 
 namespace KomplexHeat
 {
-    public class VizzyHeatSource : MonoBehaviourBase, IHeatSource, IFlightStart, IFlightUpdate
+    public class VizzyHeatSource : MonoBehaviourBase, IHeatSource, IFlightStart, IFlightFixedUpdate
     {
         private const float RunningTemperature = 340f; // Kelvin for ~67 degrees above ambient
         private const float HeatTransferCoefficient = 5f;
 
         private FlightProgramScript _flightProgramScript;
 
-        protected void OnDestroy()
+        void IFlightFixedUpdate.FlightFixedUpdate(in FlightFrameData frame)
         {
-            // Remove dangling reference.
-            var partScript = GetComponentInParent<PartScript>();
-            partScript?.OnExitHeatSource(this);
+            if (_flightProgramScript == null || _flightProgramScript.FlightProgram == null)
+            {
+                Temperature = 0f;
+                return;
+            }
+
+            Temperature = RunningTemperature;
         }
 
         void IFlightStart.FlightStart(in FlightFrameData frame)
         {
             _flightProgramScript = GetComponentInParent<FlightProgramScript>();
-        }
-
-        void IFlightUpdate.FlightUpdate(in FlightFrameData frame)
-        {
-            if (_flightProgramScript == null || _flightProgramScript.FlightProgram == null)
-            {
-                Temperature = 0f; // Temporary value
-                return;
-            }
-
-            Temperature = RunningTemperature;
         }
 
         float IHeatSource.GetHeatTransferRate(PartScript partScript)
